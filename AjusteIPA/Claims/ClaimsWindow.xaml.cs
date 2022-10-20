7d4c36a2cdea5cd91a34aa2916823980ffdfb735
Reyis Nieves <renieves@microsoft.com>
2022-10-19 23:12:19 -0400

    including the filter querys

--- AjusteIPA/Claims/ClaimsWindow.xaml.cs
+++ AjusteIPA/Claims/ClaimsWindow.xaml.cs
@@ -40,7 +40,7 @@ namespace AjusteIPA.Claims
 
             // After the data is loaded, call the DbSet<T>.Local property    
             // to use the DbSet<T> as a binding source.   
-            claimsViewSource.Source = context.Reclamaciones.Local;
+            claimsViewSource.Source = context.Reclamaciones.Local.Where(x=> x.EstatusReclamacion == "Pendiente");
             Application.Current.Dispatcher.Invoke(() =>
             {
                 Mouse.OverrideCursor = null;
