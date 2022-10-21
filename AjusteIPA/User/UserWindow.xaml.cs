309e98426c4381355e469534a3993dca4acacc88
Reyis Nieves <renieves@microsoft.com>
2022-10-20 21:33:12 -0400

    Implementing Manual del usuario.

--- AjusteIPA/User/UserWindow.xaml.cs
+++ AjusteIPA/User/UserWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace AjusteIPA.User
 {
@@ -24,6 +13,12 @@ namespace AjusteIPA.User
         {
             InitializeComponent(); 
         }
- 
+
+        private void ChromiumWebBrowser_Loaded(object sender, RoutedEventArgs e)
+        {
+            string pdfFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"User\ManualDelUsuario-SistemaIPA.pdf");
+
+            Browser.Address = pdfFilePath;
+        }
     }
 }
